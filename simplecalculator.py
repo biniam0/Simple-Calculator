8c653886673c51830400d80ebcca7b5e45da656d
bini <jaletakebede@gmail.com>
2024-06-17 01:11:36 -0900

    Adding Trigonometric features to the simple-calculator source code

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,28 +1,53 @@
+import math
+
+
 def main():
     print("Welcome to Simple Calculator")
 
     while True:
-        try:
-            num1 = float(input("Enter the first number: "))
-            operation = input("Enter the operator: ")
-            num2 = float(input("Enter the second number: "))
-            if operation == "+":
-                result = num1 + num2
-            elif operation == "-":
-                result = num1 - num2
-            elif operation == "*":
-                result = num1 * num2
-            elif operation == "/":
-                result = num1 / num2
-            else:
-                print("Invalid operation")
-                continue
+        section = input(
+            "For Trigonometry press 'T' or Arithmetics press \'A\': ")
+        if section == "T":
+            try:
+                trig = input("Enter the trig(sin, cos or tan): ")
+                value_deg = float(input("The value in radian: "))
 
-            print(result)
-        except ValueError:
-            print("Invalic input. Please enter numeric values.")
-        except ZeroDivisionError:
-            print("Division by Zero is not possible")
+                value_rad = math.radians(value_deg)
+                if trig == "sin":
+                    result = math.sin(value_rad)
+                elif trig == "cos":
+                    result = math.cos(value_rad)
+                elif trig == "tan":
+                    result = math.tan(value_rad)
+                else:
+                    print("Invalid Trig")
+                    continue
+                print("Result: ", result)
+            except ValueError:
+                print("Invalic input. Please enter numeric values.")
+
+        else:
+            try:
+                num1 = float(input("Enter the first number: "))
+                operation = input("Enter the operator: ")
+                num2 = float(input("Enter the second number: "))
+                if operation == "+":
+                    result = num1 + num2
+                elif operation == "-":
+                    result = num1 - num2
+                elif operation == "*":
+                    result = num1 * num2
+                elif operation == "/":
+                    result = num1 / num2
+                else:
+                    print("Invalid operation")
+                    continue
+
+                print("Result: ", result)
+            except ValueError:
+                print("Invalic input. Please enter numeric values.")
+            except ZeroDivisionError:
+                print("Division by Zero is not possible")
         answer = input(
             "Do you want to perform another calculation? (yes/no): ")
         if answer.lower() != "yes":
